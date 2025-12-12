29b8c2bab0c09f282fe50a87d2ea6bd30765ed4b
Samantha Morales <samanthamp43@gmail.com>
2025-12-12 15:53:37 -0600

    Complete 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    let sum = 0;
+    if (x !== parseInt(x) || y !== parseInt(y) || x < 0 || y < 0) {
+        console.log("NaN");
+        return 'ERROR';
+    } else if (x < y) {
+        for (let i = x; i <= y; i++) {
+            sum += i;
+            // console.log(i);
+        }
+        return sum;
+    } else if (y < x || y == x) {
+        for (let i = y; i <= x; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
