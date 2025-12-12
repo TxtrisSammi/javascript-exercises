d1cbe06dd3a55df31b5f655d97d557bd2027338e
Samantha Morales <samanthamp43@gmail.com>
2025-12-12 16:12:15 -0600

    Complete 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = Math.round(10 * (temp - 32) * (5/9)) / 10;
+
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = Math.round(10 * (temp * 1.8 + 32)) / 10;
+  return temp;
 };
 
 // Do not edit below this line
